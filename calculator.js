e20f16e2258179808bbe3e0e25e93af4d11f54a0
pooja suresh <Poojasurendran40@gmail.com>
2023-04-24 20:35:35 +0530

    fix

--- calculator.js
+++ calculator.js
@@ -18,10 +18,10 @@ app.post("/" , function(req,res){
     var num1 = Number(req.body.n1);
     var num2 = Number(req.body.n2);
     var result = num1 + num2;
-    res.send("The Result is  " +result);
+    res.send("Result of the calculation is " +result);
 });
 
-app.get("/bmiCalculator" , function(req,res){
+app.get("/bmiCalculator.html" , function(req,res){
 
     res.sendFile(__dirname + "/bmiCalculator.html");
 });
